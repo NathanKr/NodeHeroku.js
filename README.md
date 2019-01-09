34ba0ef21464b47e20d737cf786d06731dfe2f16
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-01-09 15:32:28 +0200

    remove file system stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 <h2>Introduction</h2>
 Small Node.js web application deployed on heroku
+
+
+<h2>Functionality</h2>
+<ul>
+<li>/about </li>
+<li></li>
+</ul>
--- server.js
+++ server.js
@@ -1,33 +1,11 @@
 console.log("app is loading");
-const fs = require("fs");
 const express = require("express");
-const fileName = "server.log";
 const process = require("process");
 
 const port = process.env.PORT || 3000;
 
 // --- create an app
 const app = express();
-
-/* 
-        next() mark that this middleware has finished
-
-        it is important to be used before the request handlers
-*/
-app.use((req, res, next) => {
-  const now = new Date().toString();
-  const info = `time : ${now} ,method : ${req.method} , url : ${req.url}`;
-  console.log(info);
-  try {
-    fs.appendFileSync(fileName, `${info}\n`);
-  } catch (err) {
-    console.log(`Error appendFileSync : ${err.message}`);
-  }
-
-  console.log(info);
-
-  next();
-});
 
 // --- handle request to /
 app.get("/", (req, res) => {
